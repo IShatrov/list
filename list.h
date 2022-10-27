0001ca12c59ac72fa0eba15068d774d2312fc54c
IShatrov <shatrov2004@mail.ru>
2022-10-27 09:14:32 +0300

    Documentation added

--- list.h
+++ list.h
@@ -8,7 +8,6 @@
 enum lst_err_codes
 {
     BAD_SIZE          = 1<<0,
-    BAD_HEAD          = 1<<1,
     BAD_TAIL          = 1<<2,
     BAD_FREE          = 1<<3,
 
@@ -38,9 +37,7 @@ struct my_list_
 
     lst_data* data;
 
-    ssize_t head;
-
-    ssize_t tail;
+    ssize_t tail; //index of last added element
 
     ssize_t free;
 
@@ -49,19 +46,53 @@ struct my_list_
 
 typedef struct my_list_ my_list;
 
+//! @param[in] lst Pointer to list.
+//! @param[in] err_code Error code.
+//! @brief Creates dump if log field of lst not NULL.
 void list_dump(my_list *lst, int err_code);
 
-//! @return Returns 1 if all OK
+//! @param[in] lst Pointer to list.
+//! @return Returns 1 if no erros were detected.
+//! @brief Checks lst for errors, calls list_dump() if an error was detected.
 char list_assert(my_list *lst);
 
+//! @param[in] lst Pointer to list.
+//! @param[in] lst_size Size of buffer.
+//! @param[in] log File to place dumps at, pass NULL to not create dumps.
+//! @param[out] lst Initiallised list.
+//! @brief Creates list.
 void list_ctor(my_list *lst, ssize_t lst_size, FILE *log);
 
+//! @param[in] lst Pointer to list.
+//! @brief Destroys list.
 void list_dtor(my_list *lst);
 
+//! @param[in] lst Pointer to list.
+//! @param[in] value Value to insert.
+//! @param[in] index Physical address to insert after.
+//! @return Returns physical address of inserted element.
+//! @brief Inserts element after certain physical address.
 ssize_t list_insert_after(my_list *lst, lst_elem value, ssize_t index);
 
+//! @param[in] lst Pointer to list.
+//! @param[in] value Value to insert.
+//! @return Returns physical address of inserted element.
+//! @brief Inserts element after last inserted element.
 ssize_t list_insert_back(my_list *lst, lst_elem value);
 
+//! @param[in] lst Pointer to list.
+//! @param[in] index Physical address to delete.
+//! @brief Deletes element.
 void list_del(my_list *lst, ssize_t index);
+
+//! @param[in] lst Pointer to list.
+//! @param[in] num Logical number.
+//! @return Returns physical address of element.
+//! @brief Transforms logical number to physical.
+ssize_t logic_to_phys(my_list *lst, ssize_t num);
+
+//! @param[in] lst Pointer to list.
+//! @brief Linearises list.
+void linearise(my_list *lst);
 
 #endif
